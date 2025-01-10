15cef9c4d56454e7f7c8780e9d12a3c5234cbed0
Mahmudjon2011 <dildooraxajayeva@gmail.com>
2025-01-10 10:12:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+// console.log(12,16);
+var ism = "Mahmud:";
+var yosh = 13;
+
+
+console.log(ism,yosh);
+
+
+
+var yosh = prompt("Ismingizni kiriting");
+var familya =  prompt("Familyangizni kiriting ");
+var yosh = prompt("Yoshingizni kirting");
+var fan = prompt("Yoqtirgan faningizni yozing");
+var tuman = prompt("Yashaydigan tumaningizni kiriting");
+
+console.log(ism,familya,yosh,fan, tuman);
+
+
+
+
+
